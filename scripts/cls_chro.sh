04029980403c375bedcef3a3b7519ce79d11607b
Michi-3000 <32291457+Michi-3000@users.noreply.github.com>
2025-09-18 02:22:50 +0800

    Update cls_chro.sh

--- scripts/cls_chro.sh
+++ scripts/cls_chro.sh
@@ -1,20 +1,13 @@
 current_time=$(date +"%Y-%m-%d-%H%M")
-for epoch in "eyeclip"
-do
-  checkpoint="checkpoint-${epoch}.pth"
-  CUDA_VISIBLE_DEVICES=0 python main_finetune_chro.py \
-    --now_epoch $epoch \
-    --test_num 5 \
-    --data_name chro \
-    --batch_size 16 \
-    --world_size 1 \
-    --model vit_large_patch16 \
+FINETUNE_CHECKPOINT="eyeclip_visual.pt"
+
+CUDA_VISIBLE_DEVICES=0 python main_finetune_chro.py \
+    --finetune "${FINETUNE_CHECKPOINT}" \
+    --clip_model_type "ViT-B/32" \
+    --batch_size 64 \
     --epochs 50 \
-    --blr 5e-3 --layer_decay 0.65 \
-    --weight_decay 0.05 --drop_path 0.2 \
-    --nb_classes 2 \
-    --data_path data/public \
-    --output_dir "output_dir_downstream/chronicdisease5/$current_time" \
-    --finetune "$checkpoint" \
-    --input_size 224
-done
+    --lr 1e-4 \
+    --weight_decay 0.01 \
+    --output_dir "./classification_results/${current_time}" \
+    --warmup_epochs 5 \
+    --test_num 5
